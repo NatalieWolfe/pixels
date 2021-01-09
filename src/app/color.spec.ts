6df0b690f564cdea6ccda67c52f0771ae71cf666
Natalie Wolfe <natalie@lifewanted.com>
2021-01-08 18:36:34 -0800

    Add Color class.

---
+++ src/app/color.spec.ts
@@ -0,0 +1,106 @@
+import { Color } from './color';
+
+describe('Color', () => {
+  it('should be constructable from a hex color', () => {
+    const c = Color.fromHex(0x010203);
+    expect(c).toBeInstanceOf(Color);
+    expect(c.r).toEqual(1);
+    expect(c.g).toEqual(2);
+    expect(c.b).toEqual(3);
+    expect(c.h).toEqual(210);
+    expect(c.s).toEqual(0.666);
+    expect(c.v).toEqual(0.011);
+  });
+
+  it('should be constructable from HSV', () => {
+    const c = Color.fromHSV(240, 0.5, 1);
+    expect(c).toBeInstanceOf(Color);
+    expect(c.r).toEqual(128);
+    expect(c.g).toEqual(128);
+    expect(c.b).toEqual(255);
+    expect(c.h).toEqual(240);
+    expect(c.s).toEqual(0.498);
+    expect(c.v).toEqual(1);
+  });
+
+  it('should be possible to change RGB values', () => {
+    const c = Color.fromHex(0x010203);
+    c.r = 5;
+    c.g = 10;
+    c.b = 15;
+    expect(c.r).toEqual(5);
+    expect(c.g).toEqual(10);
+    expect(c.b).toEqual(15);
+  });
+
+  it('should restrict RGB values', () => {
+    const c = Color.fromHex(0x000000);
+    c.r = 500;
+    c.g = 500;
+    c.b = 500;
+    expect(c.r).toEqual(255);
+    expect(c.g).toEqual(255);
+    expect(c.b).toEqual(255);
+
+    c.r = -500;
+    c.g = -500;
+    c.b = -500;
+    expect(c.r).toEqual(0);
+    expect(c.g).toEqual(0);
+    expect(c.b).toEqual(0);
+  });
+
+  it('should keep HSV in sync with RGB', () => {
+    const c = Color.fromHex(0xff0000);
+    expect(c.h).toEqual(0);
+    c.b = 255;
+    expect(c.h).toEqual(300);
+    c.r = 0;
+    expect(c.h).toEqual(240);
+
+    expect(c.s).toEqual(1);
+    c.r = 127;
+    c.g = 127;
+    expect(c.h).toEqual(240);
+    expect(c.s).toEqual(0.501);
+
+    expect(c.v).toEqual(1);
+    c.b = 128;
+    expect(c.h).toEqual(240);
+    expect(c.s).toEqual(0.007);
+    expect(c.v).toEqual(0.501);
+
+    c.r = 0;
+    c.g = 0;
+    expect(c.h).toEqual(240);
+    expect(c.s).toEqual(1);
+    expect(c.v).toEqual(0.501);
+  });
+
+  it('should keep RGB in sync with HSV', () => {
+    const c = Color.fromHSV(240, 1, 1);
+    expect(c.r).toEqual(0);
+    expect(c.g).toEqual(0);
+    expect(c.b).toEqual(255);
+
+    c.h = 210;
+    expect(c.r).toEqual(0);
+    expect(c.g).toEqual(128);
+    expect(c.b).toEqual(255);
+
+    c.h = 30;
+    expect(c.r).toEqual(255);
+    expect(c.g).toEqual(128);
+    expect(c.b).toEqual(0);
+
+    c.s = 0.5;
+    expect(c.r).toEqual(255);
+    expect(c.g).toEqual(191);
+    expect(c.b).toEqual(128);
+
+    c.v = 0.5;
+    expect(c.r).toEqual(128);
+    expect(c.g).toEqual(96);
+    expect(c.b).toEqual(64);
+  });
+});
---
+++ src/app/color.ts
@@ -0,0 +1,150 @@
+
+export type ColorLiteral = {
+  r: number;
+  g: number;
+  b: number;
+  a?: number;
+}
+
+export class Color {
+  public get r(): number { return this._r; }
+  public get g(): number { return this._g; }
+  public get b(): number { return this._b; }
+  public get a(): number { return this._a; }
+  public get h(): number { return this._h; }
+  public get s(): number { return this._s; }
+  public get v(): number { return this._v; }
+
+  public set r(r: number) {
+    this._r = Math.max(0, Math.min(255, Math.round(r)));
+    this._calculateHSV();
+  }
+  public set g(g: number) {
+    this._g = Math.max(0, Math.min(255, Math.round(g)));
+    this._calculateHSV();
+  }
+  public set b(b: number) {
+    this._b = Math.max(0, Math.min(255, Math.round(b)));
+    this._calculateHSV();
+  }
+  public set a(a: number) {
+    this._a = Math.max(0, Math.min(255, Math.round(a)));
+  }
+  public set h(h: number) {
+    this._h = Math.max(0, Math.min(359.999, Math.round(h)));
+    this._calculateRGB();
+  }
+  public set s(s: number) {
+    this._s = Math.round(Math.max(0, Math.min(1, s)) * 1000) / 1000;
+    this._calculateRGB();
+  }
+  public set v(v: number) {
+    this._v = Math.round(Math.max(0, Math.min(1, v)) * 1000) / 1000;
+    this._calculateRGB();
+  }
+
+  private _r!: number;
+  private _g!: number;
+  private _b!: number;
+  private _a!: number;
+  private _h!: number;
+  private _s!: number;
+  private _v!: number;
+
+  static fromHex(hex: number, alpha?: number): Color {
+    return new Color({
+      r: (hex >> 16) & 0x0000ff,
+      g: (hex >>  8) & 0x0000ff,
+      b: (hex >>  0) & 0x0000ff,
+      a: alpha
+    });
+  }
+
+  static fromHSV(hue: number, saturation: number, value: number): Color {
+    return new Color(Color.HSVToRGB(hue, saturation, value));
+  }
+
+  static HSVToRGB(h: number, s: number, v: number): ColorLiteral {
+    const c = s * v;
+    const x = c * (1 - Math.abs((h / 60) % 2 - 1));
+    const m = v - c;
+    let r: number;
+    let g: number;
+    let b: number;
+    if (h < 0 || h > 360) {
+      throw new Error(`Invalid hue (${h}). Must be between 0 and 360`);
+    } else if (h < 60) {
+      [r, g, b] = [c, x, 0];
+    } else if (h < 120) {
+      [r, g, b] = [x, c, 0];
+    } else if (h < 180) {
+      [r, g, b] = [0, c, x];
+    } else if (h < 240) {
+      [r, g, b] = [0, x, c];
+    } else if (h < 300) {
+      [r, g, b] = [x, 0, c];
+    } else {
+      [r, g, b] = [c, 0, x];
+    }
+    return {
+      r: Math.round((r + m) * 255),
+      g: Math.round((g + m) * 255),
+      b: Math.round((b + m) * 255)
+    };
+  }
+
+  constructor({r, g, b, a}: ColorLiteral) {
+    this._r = 0; this._g = 0; this._b = 0; this._a = 255;
+
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    if (typeof a === 'number') this.a = a;
+  }
+
+  public toHex(): string {
+    return (
+      this.r << 16 | this.g << 8 | this.b
+    ).toString(16).toUpperCase().padStart(6, '0');
+  }
+
+  public toRGB(): string {
+    return `rgb(${this.r}, ${this.g}, ${this.b})`;
+  }
+
+  public toRGBA(): string {
+    return `rgba(${this.r}, ${this.g}, ${this.b}, ${this.a})`;
+  }
+
+  public toString(format?: 'hex' | 'rgb' | 'rgba'): string {
+    if (format === 'rgb') return this.toRGB();
+    if (format === 'rgba') return this.toRGBA();
+    return this.toHex();
+  }
+
+  private _calculateHSV() {
+    const r = this.r / 255.0;
+    const g = this.g / 255.0;
+    const b = this.b / 255.0;
+    const max = Math.max(r, Math.max(g, b));
+    const min = Math.min(r, Math.min(g, b));
+    const delta = max - min;
+    let h = 0;
+    if (delta) {
+      if (max === r) h = 60 * ((g - b) / delta) + 360;
+      if (max === g) h = 60 * ((b - r) / delta) + 120;
+      if (max === b) h = 60 * ((r - g) / delta) + 240;
+      h %= 360;
+    }
+    this._h = Math.floor(h * 100) / 100;
+    this._s = Math.floor(max ? delta / max * 1000 : 0) / 1000;
+    this._v = Math.floor(max * 1000) / 1000;
+  }
+
+  private _calculateRGB() {
+    const {r, g, b} = Color.HSVToRGB(this.h, this.s, this.v);
+    this._r = r;
+    this._g = g;
+    this._b = b;
+  }
+}
