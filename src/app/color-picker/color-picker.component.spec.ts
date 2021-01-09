3d46b7b0c5d6ceb1986b0a545d19097405b50f14
Natalie Wolfe <natalie@lifewanted.com>
2021-01-08 21:10:20 -0800

    Add basic test

--- src/app/color-picker/color-picker.component.spec.ts
+++ src/app/color-picker/color-picker.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Color } from '../color';
 
 import { ColorPickerComponent } from './color-picker.component';
 
@@ -16,10 +17,21 @@ describe('ColorPickerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ColorPickerComponent);
     component = fixture.componentInstance;
+    component.color = Color.fromHex(0xff00ff);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit picked colors on change', () => {
+    let pickedColor: Color = Color.fromHex(0x000000);
+    component.picked.subscribe((color: Color) => {
+      pickedColor = color;
+    });
+
+    component.r = 127;
+    expect(pickedColor.toHex()).toEqual('7f00ff');
   });
 });
